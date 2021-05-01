1784b8fad09e4665482ef69febaef91e3622c5e3
chehsunliu <chehsunliu@gmail.com>
2021-05-02 01:31:06 +0800

    Change coding style

--- src/app/feature/header/UploadButton.tsx
+++ src/app/feature/header/UploadButton.tsx
@@ -9,17 +9,25 @@ const UploadButton = () => {
   const dispatch = useAppDispatch();
 
   const handleFileUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files !== null) {
-      const reader = new FileReader();
-
-      reader.onload = (e) => {
-        if (e.target !== null) {
-          const save = saveEditor.load(e.target.result as ArrayBuffer);
-          dispatch(gameProgressReplaced({ state: save.gameProgress }));
-        }
-      };
-      reader.readAsArrayBuffer(e.target.files[0]);
+    if (e.target.files === null) {
+      return;
     }
+
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      if (e.target === null) {
+        return;
+      }
+
+      const buffer = e.target.result;
+      if (!(buffer instanceof ArrayBuffer)) {
+        return;
+      }
+
+      const save = saveEditor.load(buffer);
+      dispatch(gameProgressReplaced({ state: save.gameProgress }));
+    };
+    reader.readAsArrayBuffer(e.target.files[0]);
   };
 
   return (
